be0befd3fcf53f534e43da71880ef0a18f1c2eca
CharaN-MAX8 <charanhadaginal@gmail.com>
2026-09-03 18:06:19 +0530

    Time: 829 ms (15.15%) | Memory: 77.3 MB (41.94%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int V = points.size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<bool> mst(V, false);
+        pq.push(make_pair(0, 0));
+        int minCost = 0;
+
+        while(!pq.empty()){
+            int u = pq.top().second;
+            int cost = pq.top().first;
+            pq.pop();
+
+            if(!mst[u]){
+                mst[u] = true;
+                minCost += cost;
+                for(int v=0; v<V; v++){
+                    if(u != v){
+                        int currCost = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                        pq.push(make_pair(currCost, v));
+                    }
+                }
+            }
+        }
+        return minCost;
+    }
+};
